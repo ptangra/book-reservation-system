8d7bde605a0ccb8845ea498a4eded0c0a2ba4da0
Petar Georgiev <petar.georgiev@milaraint.com>
2023-11-17 22:46:45 +0100

    Moved Data related classes to a book-reservation-system.Data class library project.

---
+++ book-reservation-system.Data/BooksReservationDBContext.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using book_reservation_system.Data.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace book_reservation_system.Data
+{
+    public class BooksReservationDBContext : DbContext
+    {
+        public BooksReservationDBContext(DbContextOptions options)
+            : base(options)
+        {
+            // Ensure DB is craeted - we use this so our configuration is applied to the DB on startup
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+        }
+    }
+
+    // If moved to seperate project
+    // Requires Microsoft.Extensions.Configuration & Microsoft.Extensions.Configuration.Json packages
+    public class BooksReservationDBContextFactory
+        : IDesignTimeDbContextFactory<BooksReservationDBContext>
+    {
+        public BooksReservationDBContext CreateDbContext(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<BooksReservationDBContext>();
+
+            var conn = configuration.GetConnectionString("BooksReservationDbConnectionString");
+            optionsBuilder.UseInMemoryDatabase(conn);
+            return new BooksReservationDBContext(optionsBuilder.Options);
+        }
+    }
+}
